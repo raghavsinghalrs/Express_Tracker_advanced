3ed0d6667d6d83821f4155bb095925ab85bc53e3
raghavsinghalrs <22sep2001.rs@gmail.com>
2024-03-07 01:21:49 +0530

    Making bakend for signup

---
+++ main.js
@@ -0,0 +1,10 @@
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let submit = document.getElementById('btn');
+
+submit.addEventListener('click', signup);
+
+function signup(){
+    
+}
