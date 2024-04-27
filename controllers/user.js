76b7bfac34f2d5ac5d37db8398d7b9a378dcbe9c
raghavsimghalrs <22sep2001.rss@gmail.com>
2024-04-28 03:10:34 +0530

    Added sendemail functionality

--- controllers/user.js
+++ controllers/user.js
@@ -3,47 +3,89 @@ const Expense = require('../models/expense');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Sequelize = require('../util/database');
-const brevo = require('@getbrevo/brevo');
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.API_KEY;
 
-
-const forgot = async(req,res) => {
-    try{
+const forgot = async (req, res) => {
+    try {
         const email = req.body.email;
-        const user_present = await User.findOne({where :  { email: email }});
+        const user_present = await User.findOne({ where: { email: email } });
         if(user_present){
-            console.log('User found:', user_present);
-            console.log('brevo module:', brevo);
-
-            if (brevo && brevo.ApiClient && brevo.ApiClient.instance) {
-
-                let defaultClient = brevo.ApiClient.instance;
-                let apiKey = defaultClient.authentications['api-key'];
-                apiKey.apiKey = process.env.API_KEY
-                let apiInstance = new brevo.TransactionalEmailsApi();
-                let sendSmtpEmail = new brevo.SendSmtpEmail();
-                sendSmtpEmail.subject = "My {{params.subject}}";
-                sendSmtpEmail.htmlContent = "<html><body><h1>Common: This is my first transactional email {{params.parameter}}</h1></body></html>";
-                sendSmtpEmail.sender = { "name": "raghav", "email": "22sep2001.rs@gmail.com" };
-                sendSmtpEmail.to = [
-                    { "email": "22sep2001.rs@gmail.com", "name": "sample-name" }
-                ];
-                sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
-                sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
-                apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
-                    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-                    res.status(201).json({message : 'Success'});
-                  }, function (error) {
-                    console.error(error);
+            const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+            const sender = {
+                email : '22sep2001.rs@gmail.com',
+                name : 'PAT CUMMINS',
+            };
+            const reciever = [
+                {
+                    email : email,
+                }
+            ];
+            try{
+                const sendemail = await apiInstance.sendTransacEmail({
+                    sender,
+                    to : reciever,
+                    subject : 'Testemail',
+                    textContent : 'Testemail',
                 });
-            } else {
-                throw new Error('brevo.ApiClient is not available');
+                return res.send(sendemail);
+            }catch(err){
+                console.log(err);
             }
         }
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+
+// const forgot = async(req,res) => {
+//     try{
+//         const email = req.body.email;
+//         const user_present = await User.findOne({where :  { email: email }});
+//         if(user_present){
+//             console.log('User found:', user_present);
+//             console.log('brevo module:', brevo);
+//             const apiInstance = new brevo.TransactionalEmailsApi();
+//             const apiKey = new apiInstance.authentications['apiKey'];
+//             apiKey.apiKey = process.env.API_KEY;
+//             if(!process.env.API_KEY){
+//                 return;
+//             }
+//             const sendSmtpEmail = new brevo.SendSmtpEmail();
+//             sendSmtpEmail.subject = "My {{params.subject}}";
+//             sendSmtpEmail.htmlContent = "<html><body><h1>Common: This is my first transactional email {{params.parameter}}</h1></body></html>";
+//             sendSmtpEmail.sender = { "name": "raghav", "email": "22sep2001.rs@gmail.com" };
+//             sendSmtpEmail.to = [
+//                     { "email": "22sep2001.rs@gmail.com", "name": "sample-name" }
+//                 ];
+//             sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+//             sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
+//             if(req.body?.attachments?.length){
+//                 sendSmtpEmail.attachment = attachments;
+//             }
+//             if(req.body?.cc&&req.body?.cc?.length){
+//                 sendSmtpEmail.cc = cc;
+//             }
+//             if(req.body?.bcc && req.body?.bcc?.length){
+//                 sendSmtpEmail.bcc = bcc;
+//             }
+//             const response = await apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data){
+//                 console.log('API called successfully. Returned data:'+ JSON.stringify(data));
+//                 res.status(201).json({message : 'SUCCESS'});
+//             },function(err){
+//                 console.log(err);
+//             })
+//             return response;
+//         }
+//     }catch(err){
+//         console.log(err);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// }
 
 const newuser = async(req,res) => {
     try{
